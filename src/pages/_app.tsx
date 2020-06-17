cb25f1515371c20ffd639550e0294b8a468b5682
Andrew Miracle <me@andrewmiracle.com>
2020-06-17 10:22:49 +0000

    setup chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
 import { useApollo } from '../lib/apollo'
+import theme from '../lib/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
     </ApolloProvider>
   )
 }
+
